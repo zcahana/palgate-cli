277d6c093cb82ec4fe39f3adb1f2960d87aaa06a
Zvi Cahana <zvic@il.ibm.com>
2021-07-26 00:35:47 +0300

    initial code drop

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	palgate "github.com/zcahana/palgate-lib"
+)
+
+func main() {
+	config, err := palgate.InitConfig()
+	if err != nil {
+		log.Fatalf("Error parsing configuration: %v", err)
+	}
+
+	err = config.Validate()
+	if err != nil {
+		log.Fatalf("Invalid configuration: %v", err)
+	}
+
+	client := palgate.NewClient(config)
+
+	logResp, err := client.Log()
+	if err != nil {
+		log.Fatalf("Error executing palgate command: %v", err)
+	}
+
+	if logResp.Status != palgate.ResponseStatusSuccess {
+		log.Fatalf("Error executing palgate command: status=%s, error=%s, message=%s",
+			logResp.Status, logResp.Error, logResp.Message)
+	}
+
+	err = Print(logResp.Records, os.Stdout)
+	if err != nil {
+		log.Fatalf("Error processing palgate log records: %v", err)
+	}
+}
---
+++ print.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	palgate "github.com/zcahana/palgate-lib"
+)
+
+var columns = []struct {
+	displayName   string
+	width         int
+	valueProvider func(record *palgate.LogRecord) string
+}{
+	{
+		displayName: "DATE",
+		width:       10,
+		valueProvider: func(record *palgate.LogRecord) string {
+			return record.Date()
+		},
+	},
+	{
+		displayName: "TIME",
+		width:       8,
+		valueProvider: func(record *palgate.LogRecord) string {
+			return record.Time()
+		},
+	},
+	{
+		displayName: "OPERATION TYPE",
+		width:       16,
+		valueProvider: func(record *palgate.LogRecord) string {
+			return record.Type.String()
+		},
+	},
+	{
+		displayName: "STATUS",
+		width:       10,
+		valueProvider: func(record *palgate.LogRecord) string {
+			return record.OperationStatus.String()
+		},
+	},
+	{
+		displayName: "SERIAL",
+		width:       12,
+		valueProvider: func(record *palgate.LogRecord) string {
+			return record.SerialNumber
+		},
+	},
+	{
+		displayName: "USER ID",
+		width:       14,
+		valueProvider: func(record *palgate.LogRecord) string {
+			return record.UserID
+		},
+	},
+	{
+		displayName: "NAME",
+		width:       20,
+		valueProvider: func(record *palgate.LogRecord) string {
+			return record.Name()
+		},
+	},
+}
+
+func Print(records []palgate.LogRecord, writer io.Writer) error {
+	format := buildFormatString()
+	headerArgs := buildHeaderArgs()
+
+	_, err := fmt.Fprintf(writer, format, headerArgs...)
+	if err != nil {
+		return fmt.Errorf("error writing header column: %v", err)
+	}
+
+	for _, record := range records {
+		recordArgs := buildRecordArgs(&record)
+		_, err := fmt.Fprintf(writer, format, recordArgs...)
+		if err != nil {
+			return fmt.Errorf("error writing record column: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func buildFormatString() string {
+	var builder strings.Builder
+
+	for _, column := range columns {
+		builder.WriteString(fmt.Sprintf("%%-%ds\t", column.width))
+	}
+	builder.WriteRune('\n')
+
+	return builder.String()
+}
+
+func buildHeaderArgs() []interface{} {
+	args := make([]interface{}, 0, len(columns))
+	for _, column := range columns {
+		args = append(args, column.displayName)
+	}
+
+	return args
+}
+
+func buildRecordArgs(record *palgate.LogRecord) []interface{} {
+	args := make([]interface{}, 0, len(columns))
+	for _, column := range columns {
+		args = append(args, column.valueProvider(record))
+	}
+
+	return args
+}
